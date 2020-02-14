7698e3919f738ea01f8113767ff5b93c2a8a63ac
fahmiprasanda123 <fahmiprasanda@gmail.com>
2020-02-14 14:06:14 +0700

    all not ignore

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,12 +5,23 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $faker = Faker\Factory::create(); //import library faker
+
+        $limit = 20; //batasan berapa banyak data
+
+        for ($i = 0; $i < $limit; $i++) {
+            DB::table('users')->insert([ //mengisi datadi database
+                'nama' => $faker->name,
+                'jenis_kelamin' => $faker->randomElement($array = array('male','female')), 
+            ]);
+        }
     }
 }
